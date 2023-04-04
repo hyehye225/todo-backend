9a625562f89bb938f7c876ef1a16f93938e69ce4
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-04-04 14:04:52 +0900

    24. 프로젝트 실습 #4 CRUD

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -1,11 +1,13 @@
 package com.example.todo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -91,5 +93,19 @@ public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
 	ResponseDTO<TodoDTO> response=ResponseDTO.<TodoDTO>builder().error(error).build();
 	return ResponseEntity.badRequest().body(response);
 }
+	
+}
+@DeleteMapping
+public ResponseEntity<?> delete(@RequestBody TodoDTO dto) {
+	try {
+		List<String> message=new ArrayList<>();
+		String msg=service.delete(dto.getId());
+		message.add(msg);
+		ResponseDTO<String> response=ResponseDTO.<String>builder().data(message).build();
+		return ResponseEntity.ok().body(response); 
+	}catch (Exception e) {
+		String error=e.getMessage();
+		ResponseDTO<TodoDTO> response=ResponseDTO.<TodoDTO>builder().error(error).build();
+		return ResponseEntity.badRequest().body(response);}
 }
 }
--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -45,6 +45,13 @@ public Optional<TodoEntity>updateTodo(final TodoEntity entity) {
 	});
 	return repository.findById(entity.getId());
 }
+public String delete(final String id) {
+	if(repository.existsById(id))
+		repository.deleteById(id);
+	else
+		throw new RuntimeException("id does not exist");
+	return "Deleted";
+}
 public void validate(final TodoEntity entity) {
 	if(entity==null) {
 		log.warn("Entity cannot be null.");
